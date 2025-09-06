6109da2a0cd022c3e4793c9914b3bb281afe1f42
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2025-09-05 19:34:09 -0600

    crear inicioPAM

---
+++ inicioPAM.java
@@ -0,0 +1,17 @@
+/*
+1. Crea una clase llama inicioPAM
+2. Esta clase debe contener 4 métodos que desplieguen la información
+correspondiente a la materia:
+• Reglamento POO
+• LineamientosClassroom
+• FechasdeParciales
+• PorcentajesporParcial
+3. Crea una interfaz que utilice y consuma la clase inicioPAM y sus
+métodos usando la programación orientada a objetos
+*/
+
+class inicioPAM{
+    public static void main(String[] args){
+        
+    }
+}
