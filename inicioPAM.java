e3dd21103b9d20cf647a584d880e377f957f8e6e
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2025-09-05 19:48:02 -0600

    esqueleto de funcionamiento

--- inicioPAM.java
+++ inicioPAM.java
@@ -9,9 +9,51 @@ correspondiente a la materia:
 3. Crea una interfaz que utilice y consuma la clase inicioPAM y sus
 métodos usando la programación orientada a objetos
 */
+import java.util.Scanner;
 
 class inicioPAM{
+
+    static void ReglamentoPOO(){
+        System.out.println("reglamento");
+    }
+
+    static void LineamientosClassroom(){
+        System.out.println("lineamientos");
+    }
+
+    static void FechasdeParciales(){
+        System.out.println("fechas parciales");
+    }
+
+    static void PorcentajesporParcial(){
+        System.out.println("porcentajes parcial");
+    }
+
+
     public static void main(String[] args){
-        
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("1. Reglamento POO\n2. Lineamientos de Classroom\n3. Fechas de Parciales\n4. Porcentajes por Parcial");
+        System.out.print("Seleccione una opción: ");
+        int s = teclado.nextInt();
+
+        switch(s){
+            case 1:
+                ReglamentoPOO();
+                break;
+            case 2:
+                LineamientosClassroom();
+                break;            
+            case 3:
+                FechasdeParciales();
+                break;   
+            case 4:
+                PorcentajesporParcial();
+                break;                  
+            default:
+                System.out.println("Número inválido");                           
+        }
+
+        teclado.close();
     }
 }
