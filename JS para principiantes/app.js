7ce85203de468e49a3b3406aa35b662f654839a9
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2025-09-09 11:45:09 -0600

    variables

--- JS para principiantes/app.js
+++ JS para principiantes/app.js
@@ -1,2 +1,30 @@
-document.write("<h1>Hola mundo<h1>");
+// Tipos de datos
 
+"Hola mundo" //String
+
+495 // Numero
+-3.7
+
+true
+false // bools
+
+//Array 
+["aa", "aaa", "aaa"]
+
+
+//Object
+console.log({
+    "username": "Daniel",
+    "score": 96.4,
+    "horas": 12,
+})
+
+// Var y let son para crear variables
+var usuario = "Daniel"
+let apellido = "Betancourt"
+
+// Cons es para que la variable no cambie
+const PI = 3.141519
+
+
+console.log(usuario, " ", apellido)
