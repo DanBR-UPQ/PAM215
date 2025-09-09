5e5ad4bd3ab3900437ca9dbcb7b1f4acd758c1b6
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2025-09-09 16:48:50 -0600

    Funciones

--- JS para principiantes/app.js
+++ JS para principiantes/app.js
@@ -22,9 +22,30 @@ console.log({
 // Var y let son para crear variables
 var usuario = "Daniel"
 let apellido = "Betancourt"
+let x = 5
+let y = 7
 
 // Cons es para que la variable no cambie
 const PI = 3.141519
 
 
 console.log(usuario, " ", apellido)
+
+document.write("Hola mundo") // Deprecado, elimina todo el html que ya existe
+document.getElementById("demo").innerText = "Hola mundo 2" // No deprecado, puro texto
+document.getElementById("demo2").innerHTML = "<h1>Título</h1>" // para poner HTML
+
+
+// Funciones
+function FuncionPrueba(){
+
+    console.log("Texto 1")
+    console.log("Texto 2")
+
+}
+
+function Suma(x, y){
+    return x + y;
+}
+
+document.getElementById("demo3").innerText = Suma(x,y)
