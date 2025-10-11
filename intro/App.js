58433a5ae398a43e259700f7ab12eeebe571e5f2
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2025-10-10 19:49:39 -0600

    App contador

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+// 1. Imports: Zona de importaciones
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native'; 
+import React, {useState} from 'react';
+
+
+// 2. Main: Zona de componentes
+// Ahorita es una función que cuando se compila, regresa una vista
 export default function App() {
+
+  const [contador, setContador] = useState(0); // Destructuración de useState
+
   return (
+
+
     <View style={styles.container}>
-      <Text>Hola Mundo React Native</Text>
-      <StatusBar style="auto" />
+
+      <Text>Contador: {contador}</Text>
+      <br></br>
+      <Button title='Incrementa' onPress={()=>setContador(contador+1)}> </Button>
+
+      <StatusBar style="auto" /> 
+
     </View>
+
+
   );
 }
 
+
+// 3. Styles: Zona de estilos y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
