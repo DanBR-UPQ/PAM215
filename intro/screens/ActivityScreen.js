768c13da0e4d0457b8889310d3714cfc7bc2be10
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2025-10-28 20:27:45 -0600

    Activity indicator completado

--- intro/screens/ActivityScreen.js
+++ intro/screens/ActivityScreen.js
@@ -1,13 +1,56 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { use, useState } from 'react'
+import { Text, StyleSheet, View, ActivityIndicator, Button } from 'react-native'
 
 
 export default function ActivityScreen() {
-
-    return (
-      <View>
-        <Text>proximamente por Leo</Text>
+  const [loading, setLoading] = useState(false)
+  const startLoading = () => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000);
+  }
+  /* {loading ? } */
+  if (loading){
+    return(
+      <View style={styles.container}>
+        <View style={styles.loaderContainer}>
+          <ActivityIndicator
+            size="large"
+            color="#711212ff"
+            animating={true}
+            hidesWhenStopped={true}
+          />
+          <Text style={styles.title}>Cargando...</Text>
+        </View>
       </View>
-    )
+    );
+  } else {
+    return(
+      <View style={styles.container}>
+        <Text style={styles.title}>ActivityIndicator</Text>
+        <Button title="Carga de datos" onPress={startLoading}/>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#a3a3a3ff',
+  },
+  texto: {
+    color: 'white',
+  },
+  title:{
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  loaderContainer:{
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
